bff7c432133526fcc86181d5d4090cba6150ee33
llf0029 <llf0029@users.noreply.github.com>
2016-08-05 17:12:38 +1200

    Update imgDlTest.py
    Fixed tab issue

--- imgDlTest.py
+++ imgDlTest.py
@@ -11,3 +11,4 @@ def download_image(url, save_path):
 dl_url = 'https://cdn1.epicgames.com/ue/item/Store_ForestCollection_Thumb-284x284-8c45a54fc00148e5fd677b0335cd0646.png'
 save_path = 'images/img.png'
 download_image(dl_url, save_path)
+
