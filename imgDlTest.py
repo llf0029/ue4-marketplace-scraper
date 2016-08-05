d773b683cbbb10a5edfeb1af9e65c57b9f5a22aa
llf0029 <llf0029@users.noreply.github.com>
2016-08-05 17:11:14 +1200

    Create imgDlTest.py
    Testing the ability to save an image to the disk from a website URL.

---
+++ imgDlTest.py
@@ -0,0 +1,13 @@
+#--------------------------------#
+# Testing image downloading      #
+#--------------------------------#
+
+from urllib.request import urlretrieve
+import os
+
+def download_image(url, save_path):
+	urlretrieve(url, save_path)
+
+dl_url = 'https://cdn1.epicgames.com/ue/item/Store_ForestCollection_Thumb-284x284-8c45a54fc00148e5fd677b0335cd0646.png'
+save_path = 'images/img.png'
+download_image(dl_url, save_path)
