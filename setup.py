89d6848c17befc825e4ee8e71335d1477bdfce6a
llf0029 <llf0029@users.noreply.github.com>
2016-08-09 09:35:57 +1200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='ue4mps',
-    version='0.0000001a',
-    description='Unreal Engine 4 Marketplace Scraper',
-    author='Lyndon Forsythe',
-    install_requires=['bs4', 'reportlab'],
-)
