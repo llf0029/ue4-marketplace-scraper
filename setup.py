3acded953e78586d38aaa387ea43a2283dddc10b
llf0029 <llf0029@users.noreply.github.com>
2016-08-09 08:22:57 +1200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+def setup():
+    pip install beautifulsoup4      # Used by webpage.py
+    pip install reportlab           # Used in 'pretty' output
+    
+
+if __name__ == '__main__':
+    setup()
