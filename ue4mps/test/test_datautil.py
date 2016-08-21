ff1d4c86e7cff83fe56b138ba8c923a57e1e4568
llf0029 <llf0029@users.noreply.github.com>
2016-08-22 00:12:05 +1200

    Add files via upload

---
+++ ue4mps/test/test_datautil.py
@@ -0,0 +1,166 @@
+# Imports
+import os
+import sys
+import unittest
+
+sys.path.append('../')
+
+import data_util
+
+
+class DataUtilTest(unittest.TestCase):
+
+    TEST = 'test-file'
+
+    def setUp(self):
+
+        self.du = data_util.DataUtil()
+        self.remove_test_files(self.du.full_dir)
+        self.remove_test_files(self.du.temp_dir)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.du = data_util.DataUtil()
+        cls.remove_test_files(cls, cls.du.full_dir)
+        cls.remove_test_files(cls, cls.du.temp_dir)
+
+
+    #@unittest.skip("Skipping test to speed up execution while writing tests")
+    def test_save_shelf_1(self):
+        self.du.save_shelf(self.TEST, 'test data') 
+        success = os.path.exists(
+            os.path.join(self.du.full_dir, (self.TEST + '.dat'))
+        )
+        self.assertTrue(success)
+        
+
+    def test_save_shelf_2(self):
+        self.du.save_shelf(self.TEST, ['test data 1', 'test data 2'])
+        success = os.path.exists(
+            os.path.join(self.du.full_dir, (self.TEST + '.dat'))
+        )
+        self.assertTrue(success)
+
+
+    def test_save_shelf_3(self):
+        self.du.save_shelf(self.TEST, None)
+        success = os.path.exists(
+            os.path.join(self.du.full_dir, (self.TEST + '.dat'))
+        )
+        self.assertTrue(success)
+
+
+    def test_load_shelf_1(self):
+        test_data = 'test data'
+        self.du.save_shelf(self.TEST, test_data)
+        actual = self.du.load_shelf(self.TEST)
+        self.assertEqual(actual, test_data)
+
+
+    def test_load_shelf_2(self):
+        test_data = ['test data 1', 'test data 2']
+        self.du.save_shelf(self.TEST, test_data)
+        actual = self.du.load_shelf(self.TEST)
+        self.assertEqual(actual, test_data)
+
+
+    def test_load_shelf_3(self):
+        test_data = None
+        self.du.save_shelf(self.TEST, test_data)
+        actual = self.du.load_shelf(self.TEST)
+        self.assertEqual(actual, test_data)
+
+
+    def test_overwrite_shelf(self):
+        test_data = 'overwritten!'
+        self.du.save_shelf(self.TEST, 'original')
+        self.du.save_shelf(self.TEST, test_data)
+        actual = self.du.load_shelf(self.TEST)
+        self.assertEqual(actual, test_data)
+
+
+    def test_load_no_shelf(self):
+        actual = self.du.load_shelf(self.TEST)
+        expected = {
+            'assets' : [],
+            'prices' : [],
+            'images' : [],
+            'urls'   : []
+        }
+        self.assertEqual(actual, expected)
+
+
+    def test_reset_shelf(self):
+        self.du.save_shelf(self.TEST, 'original')
+        self.du.reset_shelf(self.TEST)
+        actual = self.du.load_shelf(self.TEST)
+        expected = {
+            'assets' : [],
+            'prices' : [],
+            'images' : [],
+            'urls'   : []
+        }
+        self.assertEqual(actual, expected)
+
+
+    def test_download_image_1(self):
+        url = 'https://c1.staticflickr.com/9/8246/8560552146_6b50021122.jpg'
+        actual = self.du.download_image(url)
+        expected = os.path.join(self.du.temp_dir, 'image.png')
+        self.assertTrue(actual, expected)
+
+
+    def test_download_image_2(self):
+        with self.assertRaises(Exception) as err:
+            url = 'https://www.worldof404craft.com/404_ME.jpg'
+            self.du.download_image(url)
+
+
+    def test_download_pil_image_1(self):
+        url = 'https://c1.staticflickr.com/9/8246/8560552146_6b50021122.jpg'
+        actual = type(self.du.download_pil_image(url))
+        expected = 'Image'
+        self.assertTrue(actual, expected)
+
+
+    def test_download_pil_image_2(self):
+        with self.assertRaises(Exception) as err:
+            url = 'https://www.worldof404craft.com/404_ME.jpg'
+            self.du.download_pil_image(url)
+
+
+    def test_serialize_image(self):
+        url = 'https://c1.staticflickr.com/9/8246/8560552146_6b50021122.jpg'
+        img = self.du.download_pil_image(url)
+        actual = self.du.serialize_image(img)
+        expected_size = (300, 300)
+        expected_mode = 'RGB'
+        expected_pixels_type = type(b'bytes')
+        self.assertEqual(actual['size'], expected_size)
+        self.assertEqual(actual['mode'], expected_mode)
+        self.assertEqual(type(actual['pixels']), expected_pixels_type)
+    
+
+    def test_deserialize_image(self):
+        url = 'https://c1.staticflickr.com/9/8246/8560552146_6b50021122.jpg'
+        img = self.du.download_pil_image(url)
+        serialized_img = self.du.serialize_image(img)
+        actual = self.du.deserialize_image(serialized_img, self.TEST)
+        success = os.path.exists(
+            os.path.join(self.du.temp_dir, (self.TEST + '.png'))
+        )
+        self.assertTrue(success)
+
+
+    def remove_test_files(self, directory):
+        """Deletes the test files"""
+        for file in os.listdir(directory):
+            if file.startswith(self.TEST):
+                path = os.path.join(directory, file)
+                os.remove(path)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ ue4mps/test/test_webpage.py
@@ -0,0 +1,89 @@
+# Imports
+import sys
+import unittest
+
+sys.path.append('../')
+
+import webpage
+
+
+class WebpageTest(unittest.TestCase):
+
+    def setUp(self):
+        query_url = 'https://www.unrealengine.com/marketplace/assets?lang=&q=test'
+        base_url = 'https://www.unrealengine.com'
+        self.wp = webpage.WebPage(query_url, base_url)
+        
+
+    #@unittest.skip("Skipping test to speed up execution while writing tests")
+    def test_strip_tags(self):
+        html = '<a href="testlink.com/test">No tags <strong>here</strong>, boss</a>'
+        actual = self.wp.strip_tags(html)
+        expected = 'No tags here, boss'
+        self.assertEqual(actual, expected)
+
+
+    #@unittest.skip("Skipping test to speed up execution while writing tests")
+    def test_find_text_1(self):
+        tag = 'div'
+        class_name = 'page-title'
+        actual = self.wp.find_text(tag, class_name)
+        expected = ['Content Search']
+        self.assertEqual(actual, expected)
+
+    
+    #@unittest.skip("Skipping test to speed up execution while writing tests")
+    def test_find_text_2(self):
+        tag = 'a'
+        class_name = 'mock-ellipsis-item mock-ellipsis-item-helper'
+        actual = self.wp.find_text(tag, class_name)
+        expected = ['Crash Test Robot Dummy', '32 HDRIs Lighting Environments']
+        self.assertEqual(actual, expected)
+
+
+    #@unittest.skip("Skipping test to speed up execution while writing tests")
+    def test_find_price(self):
+        tag = 'div'
+        class_name = 'price-container'
+        actual = self.wp.find_price(tag, class_name)
+        expected = [9.99, 14.99]
+        self.assertEqual(actual, expected)
+
+
+    #@unittest.skip("Skipping test to speed up execution while writing tests")
+    def test_find_links_1(self):
+        tag = 'ul'
+        class_name = 'footer-list'
+        actual = self.wp.find_links(tag, class_name)
+        expected = ['https://www.unrealengine.com/unreal-engine-4']
+        self.assertEqual(actual, expected)
+
+
+    #@unittest.skip("Skipping test to speed up execution while writing tests")
+    def test_find_links_2(self):
+        tag = 'div'
+        class_name = 'image-box'
+        actual = self.wp.find_links(tag, class_name)
+        expected = [
+            'https://www.unrealengine.com/marketplace/crash-test-robot-dummy', 
+            'https://www.unrealengine.com/marketplace/32-hdris-lighting-environments'
+        ]
+        self.assertEqual(actual, expected)
+
+
+    #@unittest.skip("Skipping test to speed up execution while writing tests")
+    def test_find_images(self):
+        tag = 'div'
+        class_name = 'image-box'
+        actual = self.wp.find_imgs(tag, class_name)
+        expected = [
+            'https://cdn1.epicgames.com/ue/item/CrashTestRD_Thumb-284x284-1dd187177c17232e2fe7da2b4047fd4d.png',
+            'https://cdn1.epicgames.com/ue/item/HDRIs_Thumbnail-284x284-3e3950364b72f446cd86a64bde521df4.png'
+        ]
+        self.assertEqual(actual, expected)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
